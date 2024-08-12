9210005b3a2f4cd6cbaa28dab6b302eca9d74d13
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2024-08-12 03:08:27 -0700

    Add project files.

---
+++ CPW211Week5_BankAccount/Account.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPW211Week5_BankAccount
+{
+    /// <summary>
+    /// Represents a single customers bank account
+    /// </summary>
+    public class Account
+    {
+        /// <summary>
+        /// Creates an account with a specific owner and a balance of zero.
+        /// </summary>
+        /// <param name="accOwner">Customer full name that owns the account</param>
+        public Account(string accOwner)
+        {
+            Owner = accOwner;
+        }
+        /// <summary>
+        /// Account holder full name first and last
+        /// </summary>
+        public string Owner {  get; set; }
+
+        /// <summary>
+        /// The amount of money currently in the account.
+        /// </summary>
+        public double Balance { get; private set; } //can not set balance outside of the class
+
+        /// <summary>
+        /// Add a specified amount of money to the account
+        /// </summary>
+        /// <param name="amt">The positive amount to deposit</param>
+        public void Deposit(double amt)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Withdraw an amount of money from the balance
+        /// </summary>
+        /// <param name="amt">The positive amount of money to be taken from the balance</param>
+        public void Withdraw(double amt)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
