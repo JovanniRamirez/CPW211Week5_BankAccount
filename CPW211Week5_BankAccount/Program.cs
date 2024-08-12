2b3aa56ab43648e11d09ed38eab91bef02fa7e67
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2024-08-12 07:04:48 -0700

    Added some test code

--- CPW211Week5_BankAccount/Program.cs
+++ CPW211Week5_BankAccount/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CPW211Week5_BankAccount;
+
+Account myAccount = new("Joe Ortiz");
+myAccount.Deposit(500);
